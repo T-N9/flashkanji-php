acc79bfbbff32ad69b159edc741db52a7b39029e
T-N9 <thethoriumtension@gmail.com>
2023-10-22 15:21:55 +0630

    :wrench: Configuring env
    added env variables

--- config/connectDb.php
+++ config/connectDb.php
@@ -1,8 +1,22 @@
 <?php
-$servername = "localhost"; 
-$username = "root"; 
-$password = "rileythorium"; 
-$dbname = "flashkanji";
+$envFile = fopen("../.env", "r");
+
+if ($envFile) {
+    while (($line = fgets($envFile)) !== false) {
+        list($key, $value) = explode('=', $line);
+        putenv(trim($key) . '=' . trim($value));
+    }
+
+    fclose($envFile);
+} else {
+    die(".env file not found.");
+}
+
+
+$servername = getenv("DB_HOST");
+$username = getenv("DB_USER");
+$password = getenv("DB_PASS");
+$dbname = getenv("DB_NAME");
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
