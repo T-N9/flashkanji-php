d53d3a5a4814284f01ae644a4094d8d36900a962
T-N9 <thethoriumtension@gmail.com>
2023-10-26 20:53:20 +0630

    :memo: Index DOC added

--- includes/header.php
+++ includes/header.php
@@ -14,3 +14,25 @@
 </head>
 
 <body>
+    <nav class="px-4 py-2 shadow">
+        <div class="container mx-auto">
+            <div class="flex justify-between items-center">
+
+                <img width="100" src="../assets/Logo.svg" alt="Flash Kanji Logo">
+                <div class="hidden md:flex justify-center items-center space-x-4">
+                    <a href="/pages/index.php" class="text-black">Home</a>
+                    <a href="#" class="text-black">About</a>
+                    <a href="#" class="text-black">Contact</a>
+                </div>
+                <div class="md:hidden flex items-center">
+                    <button class="text-white focus:outline-none">
+                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M4 6h16M4 12h16m-7 6h7"></path>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </nav>
--- pages/index.php
+++ pages/index.php
@@ -1,5 +1,68 @@
 <?php include "../includes/header.php"; ?>
-<?php 
-    echo "Hello";
-?>
+<div class="container mx-auto py-8">
+
+    <!-- Section: FlashKanji API -->
+    <section class="mb-8">
+        <h1 class="text-4xl font-bold mb-4">⚡FlashKanji API</h1>
+        <p class="mb-4">This API provides access to a collection of Japanese Kanji characters along with their
+            associated readings and meanings. You can retrieve random Kanji characters, a specific number of Kanji, or
+            Kanji based on chapters or levels.</p>
+    </section>
+
+    <!-- Section: Base URL -->
+    <section class="mb-8">
+        <h2 class="text-2xl font-bold mb-2">Base URL</h2>
+        <code class="bg-gray-100 p-2 mb-4">https://flashkanji.000webhostapp.com/api/</code>
+    </section>
+
+    <!-- Section: Get Random Kanji -->
+    <section class="mb-8">
+        <h2 class="text-2xl font-bold mb-2">Get Random Kanji</h2>
+        <p>Retrieves a random selection of Kanji characters.</p>
+        <h3 class="text-xl font-bold mt-2 mb-1">Request</h3>
+        <code class="bg-gray-100 p-2 mb-4">GET /api/?rand={count}</code>
+        <ul class="list-disc pl-8 mb-4">
+            <li><strong>count</strong> (integer): The number of random Kanji characters to retrieve.</li>
+            <li>if <code>count === 0</code>, all kanji character data will be shuffled.</li>
+        </ul>
+    </section>
+
+    <!-- Section: Get Kanji by Limit -->
+    <section class="mb-8">
+        <h2 class="text-2xl font-bold mb-2">Get Kanji by Limit</h2>
+        <p>Retrieves a specified number of Kanji characters.</p>
+        <h3 class="text-xl font-bold mt-2 mb-1">Request</h3>
+        <code class="bg-gray-100 p-2 mb-4">GET /api/?limit={limit}&from={start}</code>
+        <ul class="list-disc pl-8 mb-4">
+            <li><strong>limit</strong> (integer): The number of Kanji characters to retrieve.</li>
+            <li><strong>from</strong> (integer): The starting index for retrieving Kanji characters.</li>
+        </ul>
+    </section>
+
+    <!-- Section: Get Kanji by Chapter -->
+    <section class="mb-8">
+        <h2 class="text-2xl font-bold mb-2">Get Kanji by Chapter</h2>
+        <p>Retrieves a set of Kanji characters based on chapters (based on Kanji Challenge Books).</p>
+        <h3 class="text-xl font-bold mt-2 mb-1">Request</h3>
+        <code class="bg-gray-100 p-2 mb-4">GET /api/?chapter={chapter}</code>
+        <ul class="list-disc pl-8 mb-4">
+            <li><strong>chapter</strong> (integer): The chapter number.</li>
+        </ul>
+    </section>
+
+    <!-- Section: Get Kanji by Level -->
+    <section class="mb-8">
+        <h2 class="text-2xl font-bold mb-2">Get Kanji by Level</h2>
+        <p>Retrieves a set of Kanji characters based on JLPT (N5, N4, N3, N2, N1) levels. This version is available only
+            N5 level for now.</p>
+        <h3 class="text-xl font-bold mt-2 mb-1">Request</h3>
+        <code class="bg-gray-100 p-2 mb-4">GET /api/?level={level}</code>
+        <ul class="list-disc pl-8 mb-4">
+            <li><strong>level</strong> (integer): The level number.</li>
+        </ul>
+    </section>
+
+</div>
+
+
 <?php include "../includes/footer.php"; ?>
